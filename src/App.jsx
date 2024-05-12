060c2963254d1a31fc615a5fcfed3a7608dd18c9
akshitcodes <116125986+akshitcodes@users.noreply.github.com>
2024-05-13 02:27:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Heading from "./components/heading"
+import React from "react"
+function App() {
+  return(
+    <>
+
+    <Heading />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/heading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Heading = (props) => {
+  return (
+    <div>
+        <h1>Hi</h1>
+    </div>
+  )
+}
+
+export default Heading
